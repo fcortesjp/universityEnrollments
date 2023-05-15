60232f1c40599ba31bc549238f4247b00f453a8d
fcortes <franciscojaviercortes@gmail.com>
2023-05-15 17:48:07 -0500

    first commit to setup git

--- Program.cs
+++ Program.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace universityEnrollments
 {
-    class Program
+    //Nombre del estudiante: Francisco Cortes
+    //Grupo: 
+    //Número y Texto del programa
+    //Código Fuente: autoría propia
+    class Student
+    {
+        // variables privadas con propiedades de Estudiante
+        private string name;
+        private string schoolProgram;
+        private char formOfPayment;
+
+        // Acceso publico a propieades
+        public string Name
+        {
+            get
+            {
+                return name; // retorna nombre 
+            }
+            set
+            {
+                name = value; // guarda nombre
+            }
+        }
+
+        public string SchoolProgram
+        {
+            get
+            {
+                return schoolProgram; // returna Programa
+            }
+            set
+            {
+                schoolProgram = value; // guarda Programa
+            }
+        }
+
+        public char FormOfPayment
+        {
+            get
+            {
+                return formOfPayment; // returna forma De Pago
+            }
+            set
+            {
+                formOfPayment = value; // guarda forma De Pago
+            }
+        }
+        class Program
     {
         static void Main(string[] args)
         {
